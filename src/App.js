2ae7b4718cb81de56c1ddae1e1c5305a920e5653
AyaElzoghby <mlak36987@gmail.com>
2024-08-26 12:40:44 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ const App = () => {
     <div className="d-flex container-fluid row my-5 m-0">
       <div className="col-12">
         <h2 className="text-center mb-3">Grid Component</h2>
-        <Grid data={data} onEdit={handleEdit} onDelete={handleDelete} />
+        <Grid data={data} onEdit={handleEdit} onDelete={handleDelete}   unVisible={["دمغه"] }  
+      />
       </div>
     </div>
   );
